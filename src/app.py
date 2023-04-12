7111cfcb76d00a148f88a8bb3e39770994c10d38
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-04-12 08:43:50 -0300

    Adição de rotas para as páginas no app.py

---
+++ src/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder="./static", template_folder="./template")
+
+@app.route("/")
+def renderIndex():
+    return render_template("index.html")
+
+@app.route("/Trabalhos")
+def renderTrabalhos():
+    return render_template("Trabalhos.html")
+
+@app.route("/Projetos")
+def renderProjetos():
+    return render_template("Projetos.html")
+
+@app.route("/Hobbies")
+def renderHobbies():
+    return render_template("Hobbies.html")
+
+@app.route("/Tecnologias")
+def renderTecnologias():
+    return render_template("Tecnologias.html")
