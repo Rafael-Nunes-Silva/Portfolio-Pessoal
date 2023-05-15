dc09a562e03832ce41225c49fa4f680eed636f32
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-05-15 15:50:21 -0300

    Separei projetos pessoais e acadêmicos

--- src/static/js/Projetos.js
+++ src/static/js/Projetos.js
@@ -1,23 +1,40 @@
 window.onload = function () {
     let projetos = document.getElementById("proj-academicos").children;
-    for(let i=1; i<projetos.length; i++)
-        projetos[i].style.display = "flex";
+    projetos[0].children[0].style.display = "block";
+    projetos[0].children[1].style.display = "none";
+    for(let i = 1; i < projetos.length; i++)
+        projetos[i].style.display = "none";
+    
     projetos = document.getElementById("proj-pessoais").children;
-    for(let i=1; i<projetos.length; i++)
+    projetos[0].children[0].style.display = "none";
+    projetos[0].children[1].style.display = "block";
+    for(let i=1; i < projetos.length; i++)
         projetos[i].style.display = "flex";
 };
 
-document.getElementById("proj-academicos-btn").addEventListener("click", function(event) {
+document.getElementById("proj-academicos-btn").addEventListener("click", function() {
+    for(let i = 0; i < 2; i++){
+        if(this.children[i].style.display == "block")
+            this.children[i].style.display = "none";
+        else this.children[i].style.display = "block";
+    }
+
     let projetos = document.getElementById("proj-academicos").children;
-    for(let i=1; i<projetos.length; i++){
+    for(let i = 1; i < projetos.length; i++){
         if(projetos[i].style.display == "flex")
             projetos[i].style.display = "none";
         else projetos[i].style.display = "flex";
     }
 });
-document.getElementById("proj-pessoais-btn").addEventListener("click", function(event) {
+document.getElementById("proj-pessoais-btn").addEventListener("click", function() {
+    for(let i = 0; i < 2; i++){
+        if(this.children[i].style.display == "block")
+            this.children[i].style.display = "none";
+        else this.children[i].style.display = "block";
+    }
+
     let projetos = document.getElementById("proj-pessoais").children;
-    for(let i=1; i<projetos.length; i++){
+    for(let i = 1; i < projetos.length; i++){
         if(projetos[i].style.display == "flex")
             projetos[i].style.display = "none";
         else projetos[i].style.display = "flex";
