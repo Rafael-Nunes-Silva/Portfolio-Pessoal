447c4101cf900eff24c9289afe0b23e45045e026
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-05-26 08:26:41 -0300

    Arrumei a forma como os json eram lidos

--- src/app.py
+++ src/app.py
@@ -6,22 +6,22 @@ app = Flask(__name__, static_folder="./static", template_folder="./templates")
 @app.route("/")
 def renderIndex():
     certificados = []
-    with open("./static/dados/certificados.json") as certificadosFile:
-        certificados = json.loads(certificadosFile.read().encode("latin_1").decode("utf_8"))["Certificados"]
+    with open("./static/dados/certificados.json", encoding="utf-8") as certificadosFile:
+        certificados = json.loads(certificadosFile.read())["Certificados"]
     return render_template("index.html", certificados = certificados)
 
 @app.route("/Trabalhos")
 def renderTrabalhos():
     trabalhos = []
-    with open("./static/dados/trabalhos.json") as trabalhosFile:
-        trabalhos = json.loads(trabalhosFile.read().encode("latin_1").decode("utf_8"))["Trabalhos"]
+    with open("./static/dados/trabalhos.json", encoding="utf-8") as trabalhosFile:
+        trabalhos = json.loads(trabalhosFile.read())["Trabalhos"]
     return render_template("Trabalhos.html", trabalhos = trabalhos)
 
 @app.route("/Projetos")
 def renderProjetos():
     projetosAcademicos, projetosPessoais = [], []
-    with open("./static/dados/projetos.json") as projetosFile:
-        fileContent = projetosFile.read().encode("latin_1").decode("utf_8")
+    with open("./static/dados/projetos.json", encoding="utf-8") as projetosFile:
+        fileContent = projetosFile.read()
         projetosAcademicos = json.loads(fileContent)["ProjetosAcademicos"]
         projetosPessoais = json.loads(fileContent)["ProjetosPessoais"]
 
@@ -34,7 +34,7 @@ def renderHobbies():
 @app.route("/Tecnologias")
 def renderTecnologias():
     tecnologias = []
-    with open("./static/dados/tecnologias.json") as tecnologiasFile:
-        tecnologias = json.loads(tecnologiasFile.read().encode("latin_1").decode("utf_8"))["Tecnologias"]
+    with open("./static/dados/tecnologias.json", encoding="utf-8") as tecnologiasFile:
+        tecnologias = json.loads(tecnologiasFile.read())["Tecnologias"]
 
     return render_template("Tecnologias.html", tecnologias = tecnologias)
